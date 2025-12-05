af0ad6f32a2a0061651bfbf0dfc786a1bd3d4455
Amaury <amauryfranssen@gmail.com>
2025-12-05 17:45:20 +0100

    feat(main): implémentation de la class Grault en relation de composition unidirectionnelle à Foo

--- src/main/java/com/jad/Grault.java
+++ src/main/java/com/jad/Grault.java
@@ -1,7 +1,23 @@
 package com.jad;
 
+// Un Grault ne peut pas être créé sans son Foo, 
+// Foo créé une collection de Grault par composition.
 public class Grault {
+    private Foo foo;
+
+    /**
+     * Construire un nouveau Grault avec son Foo.
+     * @param foo the Foo associé au Grault
+     */
+    public Grault(final Foo foo) {
+        this.foo = foo;
+    }
+
+    /**
+     * Retourne le Foo associé au Grault.
+     * @return le Foo associé au Grault
+     */
     public Foo getFoo() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return this.foo;
     }
 }
