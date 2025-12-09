88870a9ca157920e9e87b99b545a4a107f46bfb7
Amaury <amauryfranssen@gmail.com>
2025-12-09 11:19:51 +0100

    feat(Foo-corge): bidirectionnal association between Corge and Foo without infinite recursion

--- src/main/java/com/jad/Corge.java
+++ src/main/java/com/jad/Corge.java
@@ -1,14 +1,40 @@
 package com.jad;
 
 public class Corge {
+
+    private Foo foo;
+
     public Corge(final Foo foo) {
+
+        if (foo != null && foo.getCorge() != null) {
+            foo.getCorge().setFoo(null);
+        }
+      
+        this.foo = foo;
+        if (foo != null) {
+            foo.setCorge(this);
+        }
     }
 
     public Foo getFoo() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return this.foo;
     }
 
     public void setFoo(final Foo foo) {
-        throw new UnsupportedOperationException("Not implemented yet");
+
+        if (this.foo == foo) {
+            return;
+        }
+        
+        if (this.foo != null) {
+            Foo temp = this.foo;
+            this.foo = null;
+            temp.setCorge(null);
+        }
+        
+        this.foo = foo;
+        if (foo != null) {
+            foo.setCorge(this);
+        }
     }
 }
--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -8,12 +8,14 @@ public class Foo {
     private List<Grault> graults;
     private Qux qux;
     private List<Baz> bazs;
+    private Corge corge;
 
     public Foo(final Bar bar) {
         this.bar = bar;
         this.graults = new ArrayList<>();
         this.qux = new Qux();
         this.bazs = new ArrayList<>();
+        this.corge = null;
     }
 
     public Bar getBar() {
@@ -41,10 +43,25 @@ public class Foo {
     }
 
     public Corge getCorge() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return this.corge;
     }
 
-    public void setCorge(final Corge firstCorge) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public void setCorge(final Corge corge) {
+      
+        if (this.corge == corge) {
+            return;
+        }
+
+        if (this.corge != null) {
+            Corge temp = this.corge;
+            this.corge = null;
+            temp.setFoo(null);
+        }
+        
+        this.corge = corge;
+        if (corge != null) {
+            corge.setFoo(this);
+        }
+       
     }
 }
